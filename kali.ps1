0318050296b24c283f053a27f83afe6da8075636
Reskilee <112638623+Reskilee@users.noreply.github.com>
2022-10-01 09:05:10 +0900

    Add files via upload

--- kali.ps1
+++ kali.ps1
@@ -2,5 +2,5 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-l
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 wsl --install -d kali-linux
-echo install kali from store And reboot
+echo "install kali from store And reboot"
 pause
